5c517775da7721e65664c094df663e92e7bb58b0
HardWork <hardwork@HardWorkdeMacBook-Air.local>
2015-07-29 21:25:39 +0800

    Initial Commit

---
+++ helloworld/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  helloworld
+//
+//  Created by HardWork on 15/7/29.
+//  Copyright (c) 2015年 HardWork. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ helloworld/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  helloworld
+//
+//  Created by HardWork on 15/7/29.
+//  Copyright (c) 2015年 HardWork. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
